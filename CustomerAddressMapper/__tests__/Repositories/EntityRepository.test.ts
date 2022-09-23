d8d333568c8d2bde5330caf0b0bc0e8da4c0e735
Imran <imran.university@gmail.com>
2022-09-23 23:24:32 +0100

    Fixed the unit test issue. jest is now working

--- CustomerAddressMapper/__tests__/Repositories/EntityRepository.test.ts
+++ CustomerAddressMapper/__tests__/Repositories/EntityRepository.test.ts
@@ -4,37 +4,33 @@ import {describe, expect, test} from '@jest/globals';
 
 
 describe('Entity Repository', () =>{
-    // let _webApi : ComponentFramework.WebApi;
-    // let _fieldMap : AddressMap;
+    let _webApi : ComponentFramework.WebApi;
+    let _fieldMap : AddressMap;
 
-    // beforeAll(() => {
-    //     _fieldMap = {
-    //         line1 : {schemaName : 'line1'},
-    //         line2 : {schemaName : 'line2'},
-    //         line3 : {schemaName : 'line3'},
-    //         postcode : {schemaName : 'postcode'},
-    //         county : {schemaName : 'county'},
-    //         country : {schemaName : 'country'},
-    //         city : {schemaName : 'city'},
-    //         province : {schemaName : 'province'}
-    //     };
+    beforeAll(() => {
+        _fieldMap = {
+            line1 : {schemaName : 'line1'},
+            line2 : {schemaName : 'line2'},
+            line3 : {schemaName : 'line3'},
+            postcode : {schemaName : 'postcode'},
+            county : {schemaName : 'county'},
+            country : {schemaName : 'country'},
+            city : {schemaName : 'city'},
+            province : {schemaName : 'province'}
+        };
 
-    //     _webApi = {
-    //         createRecord : jest.fn(),
-    //         deleteRecord : jest.fn(),
-    //         retrieveMultipleRecords : jest.fn(),
-    //         retrieveRecord : jest.fn(),
-    //         updateRecord : jest.fn()            
-    //     }
+        _webApi = {
+            createRecord : jest.fn(),
+            deleteRecord : jest.fn(),
+            retrieveMultipleRecords : jest.fn(),
+            retrieveRecord : jest.fn(),
+            updateRecord : jest.fn()            
+        }
 
-    // })
+    })
 
-    // test('initialized', () =>{
-    //     let entityRepository = new EntityRepository(_webApi, _fieldMap);
-    //     expect(entityRepository).toBeDefined();
-    // })
-
-    test('test', () => {
-        expect( 1== 1).toBe(true);
+    test('initialized', () =>{
+        let entityRepository = new EntityRepository(_webApi, _fieldMap);
+        expect(entityRepository).toBeDefined();
     })
 })
