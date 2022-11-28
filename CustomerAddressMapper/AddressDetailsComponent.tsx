7c3eaaf8fc326d95c5a09a62ed4bd5b59cef2660
Imran <imran.university@gmail.com>
2022-11-28 21:34:11 +0000

    loading address component from passed in props

--- CustomerAddressMapper/AddressDetailsComponent.tsx
+++ CustomerAddressMapper/AddressDetailsComponent.tsx
@@ -1,4 +1,4 @@
-import { IStackStyles, Stack, TextField } from '@fluentui/react';
+import { Stack, TextField } from '@fluentui/react';
 import * as React from 'react';
 import { Address } from './Models/EntityModel';
 
@@ -9,17 +9,19 @@ interface IAddressDetailsComponentProps {
 const stackTokens = { childrenGap: 10 };
 
 const AddressDetailsComponent: React.FunctionComponent<IAddressDetailsComponentProps> = (props) => {
+  const {line1, line2, line3, postcode, city, county, province, country} = props.address
+
   return (
   <>
     <Stack tokens={stackTokens} style={{"textAlign" : "start"}} >
-        <TextField readOnly label='Line 1' value='test'></TextField>
-        <TextField readOnly label='Line 2'></TextField>
-        <TextField readOnly label='Line 3'></TextField>
-        <TextField readOnly label='Postcode'></TextField>
-        <TextField readOnly label='City'></TextField>
-        <TextField readOnly label='Province'></TextField>
-        <TextField readOnly label='County'></TextField>
-        <TextField readOnly label='Country'></TextField>
+        <TextField readOnly label='Line 1' value={line1}></TextField>
+        <TextField readOnly label='Line 2' value={line2}></TextField>
+        <TextField readOnly label='Line 3' value={line3}></TextField>
+        <TextField readOnly label='Postcode' value={postcode}></TextField>
+        <TextField readOnly label='City' value={city}></TextField>
+        <TextField readOnly label='Province' value={province}></TextField>
+        <TextField readOnly label='County' value={county}></TextField>
+        <TextField readOnly label='Country' value={country}></TextField>
     </Stack>
   </>) ;
 };
